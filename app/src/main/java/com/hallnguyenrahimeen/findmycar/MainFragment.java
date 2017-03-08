d158dd7f9557870430c96bcea60a654546d73429
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-03-08 01:16:48 -0500

    Added MapView to MainFragment with temporary pin

--- app/src/main/java/com/hallnguyenrahimeen/findmycar/MainFragment.java
+++ app/src/main/java/com/hallnguyenrahimeen/findmycar/MainFragment.java
@@ -1,29 +1,72 @@
 package com.hallnguyenrahimeen.findmycar;
 
 
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        // Inflate the layout for this fragment by using global mView
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Create map view and bind to layout
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+
+        //Test information
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("Statue of Liberty").snippet("Test pin"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.689247, -74.044502)).zoom(18).bearing(0).tilt(0).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
     }
 
 }
