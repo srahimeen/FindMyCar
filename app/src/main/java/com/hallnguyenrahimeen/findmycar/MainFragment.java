737c2d5ca2ca65b8b1ebe0cc6f77860a3b39604a
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-03-09 02:00:20 -0500

    End of workday commit

--- app/src/main/java/com/hallnguyenrahimeen/findmycar/MainFragment.java
+++ app/src/main/java/com/hallnguyenrahimeen/findmycar/MainFragment.java
@@ -11,12 +11,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -63,6 +65,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Google
         // Inflate the layout for this fragment by using global mView
         mView = inflater.inflate(R.layout.fragment_main, container, false);
         fragment_context = container.getContext();
+
         return mView;
     }
 
